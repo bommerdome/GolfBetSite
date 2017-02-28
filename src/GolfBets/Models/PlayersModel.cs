eafe3d696f43989fa4597de3a59764b73c1a0fc2
Nick <Nick@NICK-PC>
2017-02-28 15:26:31 -0500

    Add project files.

---
+++ src/GolfBets/Models/PlayersModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfBets.Models
+{
+    public class PlayersModel
+    {
+        public string playerName { get; set; }
+        public List<int> holeScores { get; set; }
+
+        public int skins { get; set; }
+    }
+}
